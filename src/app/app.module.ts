d4663cfdfe40e9c621f40146cfdca4926f02c26a
Khaled 01 <57640596+5aled01@users.noreply.github.com>
2021-05-30 15:47:08 +0000

    =D
    add log in page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,43 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {  SingupComponent } from './auth/singup/singup.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { AuthService } from './services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
+import { SinginComponent } from './auth/singin/singin.component';
+import { MenuComponent } from './menu/menu.component';
+
+const appRoute : Routes= [
+  {path :'auth/authentification' ,component:SinginComponent},
+  {path :'' ,component:SinginComponent},
+  {path :'authentification' ,component:AuthService}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingupComponent,
+    SinginComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoute)
   ],
-  providers: [],
+  providers: [
+    AuthGuardService,
+    AuthService
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
