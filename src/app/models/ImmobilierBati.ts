4156f6bb1172b6253dc2833c2a563c8a44c199ff
Khaled 01 <57640596+5aled01@users.noreply.github.com>
2021-06-14 17:07:48 +0000

    1234
    1234

--- src/app/models/ImmobilierBati.ts
+++ src/app/models/ImmobilierBati.ts
@@ -3,11 +3,10 @@ import { Point } from "./Point";
 export class ImmobilierBati {
     
     constructor(public id :number ,
-        public nom :string,
-        public idProprietaire :number,
+     
         public adresse :String ,
         public localisation : Point ,
-        public numPermie:  String,
+        public numeroPermie:  String,
         public longueur :Number,
         public  largeur :Number,
         public idProprietaire :number,
