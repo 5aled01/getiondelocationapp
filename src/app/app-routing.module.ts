95404900d3de485ce46b99ecdce2ea90335a5c13
moustapha-maker <83784274+moustapha-maker@users.noreply.github.com>
2021-06-18 03:29:36 -0700

    image  et etage service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+<<<<<<< HEAD
 import { ContratLocationComponent } from './menu/contrat-location/contrat-location.component';
+=======
+import { SingleImmoblierBatiComponent } from './menu/single-immoblier-bati/single-immoblier-bati.component';
+>>>>>>> parent of 9117ffb (Revert "Revert "08/06/2021"")
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './auth/signin/signin.component';
 import { ImmobilierBatiComponent } from './menu/immobilierBati/immobilierBati.component';
 
 import { MenuComponent } from './menu/menu.component';
+<<<<<<< HEAD
 
  
 const routes : Routes= [
@@ -12,6 +17,20 @@ const routes : Routes= [
   {path :'auth/signin' ,component: SigninComponent},
   {path :'menu' , component: MenuComponent, children:[
   
+=======
+import { ProprietairesComponent } from './menu/proprietaires/proprietaires.component';
+import { UsersComponent } from './menu/users/users.component';
+ 
+const routes : Routes= [
+ 
+  {path :'auth/signin'  ,component: SigninComponent},
+  {path :'menu' , component: MenuComponent, children:[
+    {path :'' ,redirectTo: 'home', pathMatch: 'full'},
+    {path :'home' , component: HomeComponent},
+    {path :'users' , component: UsersComponent},
+    {path :'proprietaires' , component: ProprietairesComponent},
+    {path :'detail' , component: SingleImmoblierBatiComponent},
+>>>>>>> parent of 9117ffb (Revert "Revert "08/06/2021"")
     {path :'immobilierBati' , component: ImmobilierBatiComponent},
     {path :'contrat' , component: ContratLocationComponent},
   ]},
