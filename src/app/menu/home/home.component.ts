66449288dcaca031d3da1d6799dc9561131db534
moustapha-maker <83784274+moustapha-maker@users.noreply.github.com>
2021-07-07 10:31:20 -0700

    fin vente and location

--- src/app/menu/home/home.component.ts
+++ src/app/menu/home/home.component.ts
@@ -1,5 +1,4 @@
  
- 
   
 import { NgForm } from '@angular/forms';
 import { ContratVenteService } from './../../services/contratVente.service';
@@ -28,79 +27,122 @@ export class HomeComponent implements OnInit {
 
   public reservationinternes! :Reservation[];
   public currentReservation!: Reservation;
- 
+  public annonceInternes!:AnnonceInetrne[];
   public annonceInterne!:AnnonceInetrne;
   public currentContratLocation!: ContratLocation;
   public currentContratVente!: ContratVente;
+
+
   constructor(private venteService:VenteService, private reservationService :ReservationService ,private locationService:LocationService, private annonceService:AnnonceService ,private contratLocationService:ContratLocationService,private contratVenteService:ContratVenteService) { }
 
   ngOnInit(): void {
     this.getReservation();
-    
+    this.getAnnonce();
+  }
+  getAnnonce() {
+      this.annonceService.getAllIn().subscribe(
+        (response:AnnonceInetrne[])=>{
+          this.annonceInternes=response;
+        },(error:HttpErrorResponse)=>{
+          alert(error.message);
+        }
+      )
+  }
+  getannonce(id :number):AnnonceInetrne{
+    for(let annonce of this.annonceInternes){
+      if(annonce.id==id){
+        return annonce;
+      }
+     
+    }
+     return this.annonceInterne;
+  }
+
+  getCurrentReservation(id :number){
+    for(let reservation of this.reservationinternes){
+      if(reservation.id==id){
+        return reservation;
+      }
+     
+    }
+     return null;
   }
   PasserAuVente(reservation :Reservation){
     this.currentReservation=reservation
-    this.getContratVente();
-    const newVente= new Vente(0,new Date(),this.currentContratVente?.id ,0,this.currentReservation.idClient,this.annonceInterne.idImmobilier)
+    
+    const newVente= new Vente(0,new Date(),this.getannonce(reservation.idAnnonce)?.idContrat,0,this.currentReservation.idClient,this.getannonce(reservation.idAnnonce)?.idImmobilier)
     this.venteService.addVente(newVente).subscribe(
       (response:Vente)=>{
+        this.updateEtasReservation();
       alert("vente est bien effectuer");
           },(error:HttpErrorResponse)=>{
         alert(error.message)
       }
     )
   }
-  getContratVente() {
-    this.annonceService.getAnnonceInterne(this.currentReservation.idAnnonce).subscribe(
-      (response:AnnonceInetrne)=>{
-        console.log(response)
-        this.annonceInterne=response
-          this.contratVenteService.getContratVente(response.idContrat).subscribe(
+  getContratVente():ContratVente {
+   
+          this.contratVenteService.getContratVente(this.getannonce(this.currentReservation.idAnnonce).idContrat).subscribe(
             (response:ContratVente)=>{
               console.log(response);
-              this.currentContratVente=response;
+              return response;
+              
             }
           ),(error:HttpErrorResponse)=>{
             alert(error.message);
           }
-        },(error:HttpErrorResponse)=>{
-          alert(error.message);
-        }
-      
-    )
+      return this.currentContratVente;
   }
+   gettypeAnnonce(id :number)
+   {
+     for(let annonce of this.annonceInternes){
+       if(annonce.id==id){
+           if(annonce.type=='Location'){
+             return 'Location';
+           }
+           return 'Vente';
+       }
+       
+     }
+     return ' ';
+   }
 
   public passerAuLocation(form :NgForm){
     document.getElementById('add-Location-form')?.click();
-      this.getContratLocation();
+      this.getContratLocation()
       const FormValue=form.value;
-      console.log(FormValue['dateDebut'],this.currentReservation.idClient);
-      const newLocation= new Location(0,this.currentReservation.idClient,FormValue['dateDebut'],FormValue['dateFin'],FormValue['montEncais'],this.currentContratLocation.id,this.annonceInterne.idImmobilier)
+      const newLocation= new Location(0,this.currentReservation?.idClient,FormValue['dateDebut'],FormValue['dateFin'],FormValue['montEncais'], this.getannonce(this.currentReservation.idAnnonce)?.idContrat,this.getannonce(this.currentReservation.idAnnonce)?.idImmobilier)
       this.locationService.addLocation(newLocation).subscribe(
         (response:Location)=>{
+           this.updateEtasReservation()
           alert("la location est bien ajouter")
         },(Error:HttpErrorResponse)=>{
           alert(Error.message);
         }
       )
     }
+  
+public updateEtasReservation(){
+  this.currentReservation.etats='traite';
+  this.reservationService.updateReservation(this.currentReservation).subscribe(
+    (response)=>{
 
-  public getContratLocation(){
-    this.annonceService.getAnnonceInterne(this.currentReservation.idAnnonce).subscribe(
-      (response:AnnonceInetrne)=>{
-        this.annonceInterne=response;
-          this.contratLocationService.getContratLocation(response.idContrat).subscribe(
+    },(error:HttpErrorResponse)=>{
+      alert(error.message)
+    }
+  )
+}
+
+  public getContratLocation() :ContratLocation{
+  
+          this.contratLocationService.getContratLocation(this.getannonce(this.currentReservation.idAnnonce).idContrat).subscribe(
             (response:ContratLocation)=>{
-              this.currentContratLocation=response;
+              return response;
             }
           ),(error:HttpErrorResponse)=>{
             alert(error.message);
           }
-        },(error:HttpErrorResponse)=>{
-          alert(error.message);
-        }
-      
-    )
+     return  this.currentContratLocation
   }
   public onDeleteReservation(userId: number): void {
     this.reservationService.deleteReservation(userId).subscribe(
